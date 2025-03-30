48ecaa86d04661b39ebdbb8325f20bcde1f2843b
Carolina Cretu <150701412+CarolinaCretu@users.noreply.github.com>
2025-03-30 13:29:55 +0200

    added tools

---
+++ student_association_tools-main/README.md
@@ -0,0 +1,7 @@
+# student_association_tools
+
+You can find scripts for: 
+
+1. data vizualization of members information to better understand the proportions according to the degrees/years they are enrolled in
+2. email automation
+3. newsletter template - HTML
---
+++ student_association_tools-main/email_automation/email_automation.py
@@ -0,0 +1,64 @@
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+import ssl
+import smtplib
+import pandas as pd
+
+
+email_sender = 'nextgenbiotech.fabit@gmail.com'
+email_password = input('password: ')
+
+email_receivers = ["elisafrancescaconsoli@gmail.com","lakchhanagurung888@gmail.com","sveva.grisolia@studio.unibo.it","samuele.biguzzi2@studio.unibo.it"]
+
+# path = input("path: ")
+# df = pd.read_csv(path)
+# email_receivers = list(df.Email)
+
+
+subject = 'NextGen BioTech Newsletter - February'
+
+images = [
+    {"path": "Header Newsletter.png", "cid": "header"},
+    {"path": "Recap.png", "cid": "recap"},
+    {"path": "seminar.jpg", "cid": "seminar"},
+    {"path": "Announce.png", "cid": "announcements"},
+    {"path": "Rec.png", "cid": "recommendations"},
+    {"path": "joy.jpg", "cid": "joy"},
+    {"path": "Research.png", "cid": "research"},
+    {"path": "drug.jpg", "cid": "drug"},
+    {"path": "Updated logo+name+slogan.png", "cid": "logo"}
+]
+
+
+def send_email(receivers):
+
+    for person in receivers:
+        my_email = MIMEMultipart()
+        my_email['From'] = email_sender
+        my_email['To'] = person
+        my_email['Subject'] = subject
+        
+        for image in images:
+            with open(image["path"], "rb") as img_file:
+                img = MIMEImage(img_file.read())
+                img.add_header("Content-ID",f"<{image['cid']}>")  
+                img.add_header("Content-Disposition", "inline", filename=image["path"])
+                my_email.attach(img)
+
+        with open("email_body.html", encoding="utf-8") as f:
+            html = f.read()        
+        my_email.attach(MIMEText(html, "html"))
+
+        context = ssl.create_default_context()
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+                smtp.login(email_sender, email_password)
+                smtp.sendmail(email_sender, person, my_email.as_string())
+                print(f"Email sent successfully!{person}")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+
+
+send_email(email_receivers)
